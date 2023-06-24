8815651dc8cfc357df4b623a053eb24719970599
arbiw04 <50599107+arbiw04@users.noreply.github.com>
2023-06-24 21:04:24 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://dev-film-biru-mantap.pantheonsite.io/graphql"
+	const endpoint = ""
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://dev-film-biru-mantap.pantheonsite.io/` + encodeURI(path as string)
+					`` + encodeURI(path as string)
 				}`,
 			},
 		};
@@ -31,7 +31,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://dev-film-biru-mantap.pantheonsite.io/` + encodeURI(path as string)
+					`` + encodeURI(path as string)
 				}`,
 			},
 		};
